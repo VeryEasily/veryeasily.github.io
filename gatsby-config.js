9e0e16bd9959a2e95d0b44497d739447e415183f
Luke Underwood <moresilenter@gmail.com>
2018-05-17 03:10:31 -0700

    Change color

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Luke Underwood V Personal Sitee',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
 }
