001524c19190d7a58bd746fe75eb41e7f57f8d7d
Luke Underwood <moresilenter@gmail.com>
2018-05-18 04:04:22 -0700

    Neat

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,8 +3,8 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>I've been working in tech for a while now!!</h1>
+    <p>You can find my resume <a href={resume}>here</a></p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
