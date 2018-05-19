3340259e6995898ed34b12c7d8cfcdcf7abcb4c9
Luke Underwood <moresilenter@gmail.com>
2018-05-19 09:05:30 -0700

    Fix the way stashing works hopefully

--- build.sh
+++ build.sh
@@ -6,11 +6,18 @@ _save-master() {
   [[ -d ./public ]] && rm -rf ./public
   docker-compose run --rm app gatsby build
   sudo chown -R mors:mors ./
-  git stash && stash_saved=true
+
+  echo "Clearing git stash!"
+  git stash clear && git stash
+
   git checkout master
   sudo cp -rf ./public/* ./
   sudo chown -R mors:mors ./
   git add . && git commit -am "Update"
+  git push origin master
+
+  git checkout lu/main/code
+  git stash list && echo "Recovering save from stash..." && git stash pop
 }
 
 _save-master
