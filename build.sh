895e2dc86a6b9cf7b9e8cbc7edeb849a73992c5d
Luke Underwood <moresilenter@gmail.com>
2018-05-19 08:53:12 -0700

    Fix the way stash works

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-docker-compose stop
-[[ -d ./public ]] && rm -rf ./public
-docker-compose run --rm app gatsby build
-git stash && git checkout master
-sudo cp -rf ./public/* ./
-sudo chown -R mors:mors ./
-git add . && git commit -am "Update"
-git push origin master
-git checkout lu/main/code
+
+_save-master() {
+  local stash_saved
+  docker-compose stop
+  [[ -d ./public ]] && rm -rf ./public
+  docker-compose run --rm app gatsby build
+  git stash && stash_saved=true
+  git checkout master
+  sudo cp -rf ./public/* ./
+  sudo chown -R mors:mors ./
+  git add . && git commit -am "Update"
+  git push origin master
+  git checkout lu/main/code
+  [ -z ${stash_saved+x} ] || git stash pop
+}
+
+_save-master
