b2a6b29d4a12e3e5619d2dc50bcb66589c10f0f2
Luke Underwood <moresilenter@gmail.com>
2018-05-23 03:11:16 -0700

    Trying to update build

--- build.sh
+++ build.sh
@@ -4,24 +4,39 @@ _save-master() {
   local stash_saved
   docker-compose stop
   sudo chown -R mors:mors ./
-  [[ -d ./public ]] && rm -rf ./public
+  [[ -d ./public ]] && sudo rm -rf ./public && sudo rm --rf ../public
   docker-compose run --rm app gatsby build --prefix-paths
   sudo chown -R mors:mors ./
 
-  echo "Clearing git stash!"
-  git stash clear && git stash
-
+  mv ./public ./tmp-public
   git checkout master
 
-  # Restow the build
-  stow -R public
-  stow -t $(pwd)/et-the-fog -R public
+  sudo chown -R mors:mors ./
 
-  git add -A && git commit -am "Save new build to master on $(date +"%Y-%m-%d")"
+  sudo cp -rf ./tmp-public/* ./public/
+
+  sudo chown -R mors:mors ./
+
+  git add ./public
+
+  git commit -m "Updating site"
+
   git push origin master
 
-  git checkout dev
-  git stash list && echo "Recovering save from stash..." && git stash pop
+  git checkout dev 
+
+  sudo chown -R mors:mors ./
+
+
+  # Restow the build
+  # stow -R public
+  # stow -t $(pwd)/et-the-fog -R public
+
+  # git add -A && git commit -am "Save new build to master on $(date +"%Y-%m-%d")"
+  # git push origin master
+
+  # git checkout dev
+  # git stash list && echo "Recovering save from stash..." && git stash pop
 }
 
 _save-master
