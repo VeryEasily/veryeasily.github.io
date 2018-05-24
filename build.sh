8ec43b32a2990e4a779b6adc608094d0bceaa762
Luke Underwood <moresilenter@gmail.com>
2018-05-24 15:20:48 -0700

    Update master

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+docker-compose stop
+sudo rm -rf ./{public,tmp-public}
+sudo chown -R mors:mors ./
 
 _save-master() {
   local stash_saved
-  docker-compose stop
-  sudo chown -R mors:mors ./
-  [[ -d ./public ]] && sudo rm -rf ./public && sudo rm --rf ../public
   docker-compose run --rm app gatsby build --prefix-paths
   sudo chown -R mors:mors ./
 
@@ -19,25 +19,25 @@ _save-master() {
 
   git add ./public
 
-  git commit -m "Updating site"
+  # git commit -m "Updating site"
 
-  git push origin master
-
-  git checkout dev 
-
-  sudo rm -rf ./tmp-public
-
-  sudo chown -R mors:mors ./
-
-  # Restow the build
-  # stow -R public
-  # stow -t $(pwd)/et-the-fog -R public
-
-  # git add -A && git commit -am "Save new build to master on $(date +"%Y-%m-%d")"
   # git push origin master
 
-  # git checkout dev
-  # git stash list && echo "Recovering save from stash..." && git stash pop
+  # git checkout dev 
+
+  # sudo rm -rf ./tmp-public
+
+  # sudo chown -R mors:mors ./
+
+  # # Restow the build
+  # # stow -R public
+  # # stow -t $(pwd)/et-the-fog -R public
+
+  # # git add -A && git commit -am "Save new build to master on $(date +"%Y-%m-%d")"
+  # # git push origin master
+
+  # # git checkout dev
+  # # git stash list && echo "Recovering save from stash..." && git stash pop
 }
 
 _save-master
