333f5fa0cf31be4c522f42c9e59a463ecdae08dd
Luke Underwood <veryveryeasily@gmail.com>
2018-05-30 07:47:56 -0700

    Remove the artwork I don't like as much

--- src/components/artworks.js
+++ src/components/artworks.js
@@ -5,7 +5,7 @@ import classnames from 'classnames'
 
 const IMG_LIST = [
   'https://c1.staticflickr.com/6/5338/9288760247_6153e06904_c.jpg',
-  'https://c1.staticflickr.com/4/3741/9288774947_77bc0a0142_h.jpg',
+  // 'https://c1.staticflickr.com/4/3741/9288774947_77bc0a0142_h.jpg',
   'https://c1.staticflickr.com/6/5500/9288758335_d9258bf52b_h.jpg',
   'https://c1.staticflickr.com/8/7404/9288761047_22fa63c71b_h.jpg',
   'https://c1.staticflickr.com/8/7383/9291545346_1ca204e760_h.jpg'
