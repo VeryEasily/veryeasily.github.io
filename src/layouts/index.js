e13b99e059adc91b2f7ad278042d0123ec109d77
Luke Underwood <veryveryeasily@gmail.com>
2018-05-29 21:07:05 -0700

    Starting to style

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-import '../scss/index.css'
+import '../scss/index.scss'
 
 const Layout = ({ children, data }) => (
-  <div>
+  <div className="site">
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
@@ -14,15 +14,10 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className="site__header">
+      <Header siteTitle={data.site.siteMetadata.title} />
+    </div>
+    <div className="site__body main-content">
       {children()}
     </div>
   </div>
