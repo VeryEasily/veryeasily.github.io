a477d04b43523c49d5cde958d48f4a66ea4245fc
Luke Underwood <veryveryeasily@gmail.com>
2018-05-30 09:14:47 -0700

    New stuff

--- public/path----0d52784c4f47798fbeba.js
+++ public/path----0d52784c4f47798fbeba.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{105:function(t,e){t.exports={data:{site:{siteMetadata:{title:"veryeasily"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{106:function(t,e){t.exports={data:{site:{siteMetadata:{title:"veryeasily"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----0d52784c4f47798fbeba.js.map
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Artworks, { IMG_LIST } from '../components/artworks'
+import styles from '../styles/art.module.scss'
 
 const SecondPage = () => {
   return (
     <div>
-      <h1>artwork here:</h1>
+      <h1 className={styles.second_title}>artwork here:</h1>
       <Artworks />
     </div>
   )
